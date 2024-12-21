f60efea4ce0f97983242e27a14a0123b7573c5fc
D3F4ULT <166531672+hdgdjk@users.noreply.github.com>
2024-12-21 09:59:43 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+let myForm = document.forms[0];
+let userName = myForm.username;
+let textArea = comments;
+let button = btn;
+let email = mail;
+let myRegExpe = new RegExp(email.pattern)
+function validateForm(){
+    
+}
